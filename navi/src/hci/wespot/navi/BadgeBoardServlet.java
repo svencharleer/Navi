6d870b9e2e9c70cd41231eadb0fa13b15e6c1b8f
Sven Charleer <sven.charleer@gmail.com>
2013-02-14 16:39:55 +0100

    basic user list. and badge per user view

--- navi/src/hci/wespot/navi/BadgeBoardServlet.java
+++ navi/src/hci/wespot/navi/BadgeBoardServlet.java
@@ -1,11 +1,16 @@
 package hci.wespot.navi;
 import java.io.IOException;
 import java.util.*;
+import java.util.logging.Level;
+
+import com.google.appengine.api.memcache.*;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.lang.reflect.Type;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 
 
@@ -13,33 +18,77 @@ import java.lang.reflect.Type;
 public class BadgeBoardServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		
-		//GET call
-		String returnValue = WebHelpers.get("http://openbadges-hci.appspot.com/rest/getinfo/badgesusers");
-		//String returnValue = get("http://navi-hci.appspot.com/BADGES.JSON");
+		Collection<JoseBadge> badges = getBadgeData();
+		
+		String pUserName = req.getParameter("username");
+		if(pUserName != null && pUserName.compareTo("") != 0)
+		{
+			pUserName = URLDecoder.decode(pUserName, "UTF-8");
+			//Iterate and generate display badges
+			Iterator<JoseBadge> it = badges.iterator();
+			Collection<BadgeForDisplay> displayBadges = new ArrayList<BadgeForDisplay>();
+			while(it.hasNext())
+			{
+				JoseBadge badge = (JoseBadge)it.next();
+				if(badge.recipient == null)
+					continue;
+				if(badge.recipient != null && badge.recipient.compareTo(pUserName) != 0)
+					continue;
+	
+				//Add badge to list
+				BadgeForDisplay displayBadge = new BadgeForDisplay();
+				displayBadge.description = badge.badge.description;
+				displayBadge.imageUrl =  "http://openbadges-hci.appspot.com"+badge.badge.image;
+				displayBadge.name = badge.badge.name;
+				displayBadge.url = "http://openbadges-hci.appspot.com/rest/getinfo/id/" + badge.id;
+				displayBadges.add(displayBadge);
+			}
+			//add to session and bail
+			req.getSession().setAttribute("name", pUserName);
+			req.getSession().setAttribute("badges", displayBadges);
+			RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/BadgeBoard_user.jsp");
+			if(dispatch != null)
+				dispatch.forward(req, resp);
+		
+		}
+		else
+		{
+			//display user name list
+			//Iterate and generate display badges
+			Iterator<JoseBadge> it = badges.iterator();
+			Collection<String> userNames = new ArrayList<String>();
+			while(it.hasNext())
+			{
+				JoseBadge badge = (JoseBadge)it.next();
+				if(!userNames.contains(badge.recipient))
+					userNames.add(badge.recipient);		
+			}
+		
+			//add to session and bail
+			req.getSession().setAttribute("userNames", userNames);
+			RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/BadgeBoard.jsp");
+			if(dispatch != null)
+				dispatch.forward(req, resp);
+		}
+		
+	}
 
+	private Collection<JoseBadge> getBadgeData() {
+		//GET call
+		MemcacheService syncCache = MemcacheServiceFactory.getMemcacheService();
+	    syncCache.setErrorHandler(ErrorHandlers.getConsistentLogAndContinue(Level.INFO));
+	    String returnValue = (String) syncCache.get("badgesusers");
+		if(returnValue == null)
+		{
+			returnValue = WebHelpers.get("http://openbadges-hci.appspot.com/rest/getinfo/badgesusers");
+			syncCache.put("badgesusers", returnValue,Expiration.byDeltaSeconds(300));
+		}
+		
 		//JSON conversion
 		Gson json = new Gson();
 		Type returnType = new TypeToken<Collection<JoseBadge>>(){}.getType();
 		Collection<JoseBadge> badges = (Collection<JoseBadge>)json.fromJson(returnValue, returnType);
-		
-		//TODO: guess we should cache this. gapp has something to cache no?
-		
-		//Iterate and generate display badges
-		Iterator<JoseBadge> it = badges.iterator();
-		Collection<String> userNames = new ArrayList<String>();
-		while(it.hasNext())
-		{
-			JoseBadge badge = (JoseBadge)it.next();
-			userNames.add(badge.recipient);		
-		}
-	
-		//add to session and bail
-		req.getSession().setAttribute("badges", userNames);
-		RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/BadgeOverview.jsp");
-		if(dispatch != null)
-			dispatch.forward(req, resp);
-    	
-		
+		return badges;
 	}
 	
 	
---
+++ navi/war/BadgeBoard.jsp
@@ -0,0 +1,60 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="com.google.appengine.api.users.User" %>
+<%@ page import="com.google.appengine.api.users.UserService" %>
+<%@ page import="com.google.appengine.api.users.UserServiceFactory" %>
+<%@ page import="java.util.Collection" %>
+<%@ page import="hci.wespot.navi.*" %>
+<%@ page import="java.util.*" %>
+<%@ page import="java.net.URLEncoder" %>
+
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<html>
+<head>
+<title>CHI13 Badge Overview</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+	<script src="http://beta.openbadges.org/issuer.js"></script>
+	<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-38498955-1']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+	</script>
+</head>
+<body>
+<div data-role="page" data-theme="b">
+	<div data-role="header">
+		CHI13 Badge Board
+	</div>
+<!-- /header -->
+
+	<div data-role="content" style="background:#fff;">
+	<ul data-role="listview" data-inset="true" data-filter="true">
+	<%
+	
+	
+		Collection<String> userNames = (Collection<String>)request.getSession().getAttribute("userNames");
+		Iterator it = userNames.iterator();
+		while(it.hasNext())
+		{
+			String name = (String)it.next();
+	%>
+		<li  data-icon="false">
+		<a href="badgeboard?username=<%= URLEncoder.encode(name, "UTF-8") %>"><%= name %></a>
+		</li>
+	
+	<%  
+	    }
+	%>
+	</ul>
+	</div>
+</div>
+</body>
+</html>
